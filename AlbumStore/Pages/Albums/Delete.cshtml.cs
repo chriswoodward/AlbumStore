8000af193e742cba96c409ec2b5fa6cbfc910b14
Chris Woodward <Chris.Woodward@objectiveit.com>
2020-01-21 11:59:42 +0000

    Added DELETE Album functionality.

--- AlbumStore/Pages/Albums/Delete.cshtml.cs
+++ AlbumStore/Pages/Albums/Delete.cshtml.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using AlbumStore.Data;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace AlbumStore
 {
@@ -16,42 +14,69 @@ namespace AlbumStore
         public DeleteModel(AlbumStore.Data.AlbumStoreDbContext context)
         {
             _context = context;
-        }
+        }       
 
         [BindProperty]
-        public Album Album { get; set; }
+        public DeleteAlbumViewModel Album { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public class DeleteAlbumViewModel
         {
-            if (id == null)
+            public int Id { get; set; }
+
+            [Required, StringLength(160)]
+            public string Title { get; set; }
+
+            [Required]
+            [Display(Name = "Artist")]            
+
+            public string Artist { get; set; }
+
+            [Required, StringLength(255)]
+            public string ReferenceNumber { get; set; }
+        }
+
+
+        public IActionResult OnGet(int? albumId)
+        {
+            if (albumId == null)
             {
                 return NotFound();
             }
 
-            Album = await _context.Albums
-                .Include(a => a.Artist).FirstOrDefaultAsync(m => m.AlbumId == id);
+            var album = _context.Albums.Include(x => x.Artist).SingleOrDefault(x => x.AlbumId == albumId);
 
-            if (Album == null)
+            if (album == null)
             {
                 return NotFound();
             }
+
+            Album = new DeleteAlbumViewModel()
+            {
+                Id = album.AlbumId,
+                Title = album.Title,
+                Artist = album.Artist.Name,
+                ReferenceNumber = album.ReferenceNumber
+            };
+
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        public IActionResult OnPost(int? albumId)
         {
-            if (id == null)
+            if (albumId == null)
             {
                 return NotFound();
             }
 
-            Album = await _context.Albums.FindAsync(id);
+            var album = _context.Albums.Find(albumId);
 
-            if (Album != null)
+            if (album == null)
             {
-                _context.Albums.Remove(Album);
-                await _context.SaveChangesAsync();
+                return RedirectToPage("./NotFound");
             }
+
+            _context.Albums.Remove(album);
+            _context.SaveChanges();
 
             return RedirectToPage("./Index");
         }
